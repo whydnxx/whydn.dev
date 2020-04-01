7e0b8ed777d21594b2ff64db54be5224a5d6ca33
Wahyudin <whydn.id@hotmail.com>
2020-04-01 23:26:00 +0700

    Add theme switcher

--- src/components/header.js
+++ src/components/header.js
@@ -3,37 +3,85 @@ import React from "react"
 import Language from "./language"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import HeaderLayout from "./headerLayout"
+import Toggle from "./Toggle";
+import sun from "../images/sun.png";
+import moon from "../images/moon.png";
 
-const Header = () => {
-  return (
-    <HeaderLayout>
-      <div className="flex flex-col items-center justify-center sm:flex-row">
-        <nav className="mb-1 sm:mb-0">
-          <AnchorLink
-            href="#work"
-            className="cursor-pointer hover:text-primary dark:hover:text-secondary"
-          >
-            <FormattedMessage id="nav.work" />
-          </AnchorLink>
-          <AnchorLink
-            href="#about"
-            className="mx-6 cursor-pointer md:mx-8 hover:text-primary dark:hover:text-secondary"
-          >
-            <FormattedMessage id="nav.about" />
-          </AnchorLink>
-          <AnchorLink
-            href="#contact"
-            className="cursor-pointer hover:text-primary dark:hover:text-secondary"
-          >
-            <FormattedMessage id="nav.contact" />
-          </AnchorLink>
-        </nav>
-        <div>
-          <Language />
-        </div>
-      </div>
-    </HeaderLayout>
-  )
-};
+class Header extends React.Component {
+  state = {
+    theme: null,
+  };
+  componentDidMount() {
+    this.setState({ theme: window.__theme });
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme });
+    };
+  }
 
+  render() {
+    return (
+        <HeaderLayout>
+          <div className="flex flex-col items-center justify-center sm:flex-row">
+            <nav className="mb-1 sm:mb-0">
+              <AnchorLink
+                  href="#work"
+                  className="cursor-pointer hover:text-primary dark:hover:text-secondary"
+              >
+                <FormattedMessage id="nav.work" />
+              </AnchorLink>
+              <AnchorLink
+                  href="#about"
+                  className="mx-6 cursor-pointer md:mx-8 hover:text-primary dark:hover:text-secondary"
+              >
+                <FormattedMessage id="nav.about" />
+              </AnchorLink>
+              <AnchorLink
+                  href="#contact"
+                  className="cursor-pointer hover:text-primary dark:hover:text-secondary"
+              >
+                <FormattedMessage id="nav.contact" />
+              </AnchorLink>
+            </nav>
+            <div>
+              <Language />
+            </div>
+            <div className="flex justify-between sm:ml-2 sm:ml-6">
+              {this.state.theme !== null ? (
+                  <Toggle
+                      icons={{
+                        checked: (
+                            <img
+                                src={moon}
+                                width="16"
+                                height="16"
+                                role="presentation"
+                                style={{ pointerEvents: 'none' }}
+                            />
+                        ),
+                        unchecked: (
+                            <img
+                                src={sun}
+                                width="16"
+                                height="16"
+                                role="presentation"
+                                style={{ pointerEvents: 'none' }}
+                            />
+                        ),
+                      }}
+                      checked={this.state.theme === 'dark'}
+                      onChange={e =>
+                          window.__setPreferredTheme(
+                              e.target.checked ? 'dark' : 'light'
+                          )
+                      }
+                  />
+              ) : (
+                  <div style={{ height: '24px' }} />
+              )}
+            </div>
+          </div>
+        </HeaderLayout>
+    );
+  }
+}
 export default injectIntl(Header)
