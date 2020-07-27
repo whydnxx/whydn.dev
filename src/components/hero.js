cf647f461af81e0cb8399508bcd7938b2cc92a25
Wahyudin <whydn.id@hotmail.com>
2020-07-27 10:05:02 +0700

    Fix: React.lazy and Suspense on client side only

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,15 +6,19 @@ const Avatar = lazy(() => import("./avatar"))
 const renderLoader = () => <p>Loading</p>
 
 const Hero = () => {
+  const isSSR = typeof window === "undefined"
+
   return (
     <section className="flex flex-wrap max-w-md px-6 py-16 mx-auto sm:max-w-xl md:max-w-4xl lg:max-w-5xl md:py-24 lg:py-40">
       <div className="flex items-center justify-center w-full md:w-1/3">
-        <Suspense fallback={renderLoader()}>
-          <Avatar
-            className="w-40 transition duration-500 ease-in-out transform sm:w-44 md:w-48 lg:w-64 hover:scale-110 avatar"
-            alt="whydn avatar version"
-          />
-        </Suspense>
+        {!isSSR && (
+          <Suspense fallback={renderLoader()}>
+            <Avatar
+              className="w-40 transition duration-500 ease-in-out transform sm:w-44 md:w-48 lg:w-64 hover:scale-110 avatar"
+              alt="whydn avatar version"
+            />
+          </Suspense>
+        )}
       </div>
       <div className="flex items-center mt-6 md:w-2/3 md:mt-0 md:pl-3 lg:pl-6">
         <div className="flex flex-col items-center text-primary dark:text-secondary md:items-start">
