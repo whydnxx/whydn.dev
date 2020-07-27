5c3ae27876a12b3b07d76350005f7c648069e5ba
Wahyudin <whydn.id@hotmail.com>
2020-07-27 09:31:27 +0700

    Improve : Code splitting with React.lazy and Suspense

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,18 +1,20 @@
-import React from "react"
-import Button from './button';
+import React, { lazy, Suspense } from "react"
+import Button from "./button"
 
 import { FormattedMessage, injectIntl, useIntl } from "gatsby-plugin-intl"
-import Avatar from "./avatar";
+const Avatar = lazy(() => import("./avatar"))
+const renderLoader = () => <p>Loading</p>
 
 const Hero = () => {
-
   return (
     <section className="flex flex-wrap max-w-md px-6 py-16 mx-auto sm:max-w-xl md:max-w-4xl lg:max-w-5xl md:py-24 lg:py-40">
       <div className="flex items-center justify-center w-full md:w-1/3">
-        <Avatar
+        <Suspense fallback={renderLoader()}>
+          <Avatar
             className="w-40 transition duration-500 ease-in-out transform sm:w-44 md:w-48 lg:w-64 hover:scale-110 avatar"
             alt="whydn avatar version"
-        />
+          />
+        </Suspense>
       </div>
       <div className="flex items-center mt-6 md:w-2/3 md:mt-0 md:pl-3 lg:pl-6">
         <div className="flex flex-col items-center text-primary dark:text-secondary md:items-start">
