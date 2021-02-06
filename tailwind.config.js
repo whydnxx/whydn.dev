773cd5f35f8ad7f275385a6b127089540538bb7a
Wahyudin <whydn.id@hotmail.com>
2021-02-07 00:54:28 +0700

    Initialize Project ✨

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
